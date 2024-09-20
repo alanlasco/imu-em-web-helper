a7ee9339cc283aca0380879da9dd3e94b3b33d8b
alanlasco <61526254+alanlasco@users.noreply.github.com>
2024-09-20 00:37:50 -0300

    adding a winning, later to be a component

--- src/components/ListOfWinners.tsx
+++ src/components/ListOfWinners.tsx
@@ -3,16 +3,29 @@ import React, { useState } from "react";
 export const ListOfWinners = () => {
   const [goldCredits, setGoldCredits] = useState<number | string>(200);
   const [imes, setImes] = useState<number | string>(1);
+  const [winners, setWinners] = useState<
+    { goldCredits: number; imes: number }[]
+  >([]);
 
   const handleGoldCreditsChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
-    setGoldCredits(value === "" ? "" : Number(value)); // No cambiar a número si está vacío
+    setGoldCredits(value === "" ? "" : Number(value));
   };
 
   const handleImesChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
     setImes(value === "" ? "" : Number(value));
   };
+
+  const addWinner = () => {
+    if (typeof goldCredits === "number" && typeof imes === "number") {
+      setWinners([
+        ...winners,
+        { goldCredits: Number(goldCredits), imes: Number(imes) },
+      ]);
+    }
+  };
+
   return (
     <>
       <article className="configWinner">
@@ -21,9 +34,8 @@ export const ListOfWinners = () => {
           <input
             type="number"
             name="goldCredits"
-            id=""
             value={goldCredits}
-            onChange={handleGoldCreditsChange} //e argumento que representa el cambio del input
+            onChange={handleGoldCreditsChange}
           />
           <label htmlFor="imes">Amount of IMES: </label>
           <input
@@ -32,12 +44,16 @@ export const ListOfWinners = () => {
             value={imes}
             onChange={handleImesChange}
           />
+
+          <button onClick={addWinner}>Add Winner</button>
         </span>
       </article>
       <section className="List">
-        <span>
-          Winner {Number(goldCredits) * 2} {imes}
-        </span>
+        {winners.map((winner, index) => (
+          <span key={index}>
+            Winner {winner.goldCredits * 2} {winner.imes} <br />
+          </span>
+        ))}
       </section>
     </>
   );
