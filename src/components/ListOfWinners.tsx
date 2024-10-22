454d26b863c1cc3630c44113c6b132f6b917e922
alanlasco <61526254+alanlasco@users.noreply.github.com>
2024-10-22 19:29:33 -0300

    cards for winner's list

--- src/components/ListOfWinners.tsx
+++ src/components/ListOfWinners.tsx
@@ -62,65 +62,79 @@ export const ListOfWinners = () => {
   return (
     <>
       <article className="configWinner">
-        <div className="titleContainer">
-          <img className="muImg" src={mu} alt="MU Light" />
-          <span className="titleSection">List your Winners:</span>
+        <div className="greenLight"></div>
+        <div className="container">
+          <div className="titleContainer">
+            <img className="muImg" src={mu} alt="MU Light" />
+            <span className="titleSection">List your Winners:</span>
+          </div>
+          <p className="description">Set Multipliers: </p>
+          <span>
+            <label htmlFor="goldCredits">Amount of GC: </label>
+            <input
+              className="inputGoldCredits"
+              type="number"
+              name="goldCredits"
+              value={goldCredits}
+              onChange={handleGoldCreditsChange}
+            />
+            <label htmlFor="imes">Amount of IMES: </label>
+            <input
+              className="inputImes"
+              type="number"
+              name="imes"
+              value={imes}
+              onChange={handleImesChange}
+            />
+          </span>
         </div>
-        <p className="description">Set the Multipliers: </p>
-        <span>
-          <label htmlFor="goldCredits">Amount of GC: </label>
-          <input
-            className="inputGoldCredits"
-            type="number"
-            name="goldCredits"
-            value={goldCredits}
-            onChange={handleGoldCreditsChange}
-          />
-          <label htmlFor="imes">Amount of IMES: </label>
-          <input
-            className="inputImes"
-            type="number"
-            name="imes"
-            value={imes}
-            onChange={handleImesChange}
-          />
-        </span>
       </article>
 
       <div className="winnersContainer">
-        <section className="List">
-          <span>
-            <p className="titleSectionSmaller">Winners List:</p>
-            <button onClick={addWinner}>Add Winner</button>
-          </span>
-          <div className="textContainer">
-            <span className="nameText">Winner: </span>
-            <span className="timesText">T/: </span>
-            <span className="eventsText">Event/s: </span>
-          </div>
-          {winners.map((winner, index) => (
-            <Winner
-              key={index}
-              onWinnerChange={(name, times, event, goldCredits, imes) =>
-                handleWinnerChange(index, name, times, event, goldCredits, imes)
-              }
-              goldCredits={Number(goldCredits)} // Asegúrate de que sea un número
-              imes={Number(imes)} // Asegúrate de que sea un número
-            />
-          ))}
-        </section>
-
-        <section className="WinnersOutput">
-          <p className="titleSectionSmaller">All Winners:</p>
-          {winners.map((winner, index) => (
-            <div key={index}>
-              <p className="description">
-                Name: {winner.name} | Event: {winner.event} | Times Won:{" "}
-                {winner.times} | GC: {winner.goldCredits} | IMES: {winner.imes}
-              </p>
+        <div className="greenLight"></div>
+        <div className="container">
+          <section className="List">
+            <span>
+              <p className="titleSectionSmaller">Winners List:</p>
+              <button onClick={addWinner}>Add Winner</button>
+            </span>
+            <div className="textContainer">
+              <span className="nameText">Winner: </span>
+              <span className="timesText">T/: </span>
+              <span className="eventsText">Event/s: </span>
             </div>
-          ))}
-        </section>
+            {winners.map((winner, index) => (
+              <Winner
+                key={index}
+                onWinnerChange={(name, times, event, goldCredits, imes) =>
+                  handleWinnerChange(
+                    index,
+                    name,
+                    times,
+                    event,
+                    goldCredits,
+                    imes
+                  )
+                }
+                goldCredits={Number(goldCredits)} // Asegúrate de que sea un número
+                imes={Number(imes)} // Asegúrate de que sea un número
+              />
+            ))}
+          </section>
+
+          <section className="WinnersOutput">
+            <p className="titleSectionSmaller">All Winners:</p>
+            {winners.map((winner, index) => (
+              <div key={index}>
+                <p className="description">
+                  Name: {winner.name} | Event: {winner.event} | Times Won:{" "}
+                  {winner.times} | GC: {winner.goldCredits} | IMES:{" "}
+                  {winner.imes}
+                </p>
+              </div>
+            ))}
+          </section>
+        </div>
       </div>
     </>
   );
